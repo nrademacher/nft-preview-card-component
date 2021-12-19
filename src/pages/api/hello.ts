bc52f0b2a3bc36b57110904941e1fcd086890f2a
Nikolay Rademacher <rademacher.nikolay@gmail.com>
2021-12-19 02:09:50 +0100

    feat: basic setup and structure

--- src/pages/api/hello.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next"
-
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-const hello = (req: NextApiRequest, res: NextApiResponse) => {
-    res.statusCode = 200
-    res.json({ name: `John Doe` })
-}
-
-export default hello
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,27 @@ module.exports = {
     content: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
     darkMode: "media", // or false or 'class'
     theme: {
-        extend: {},
+        fontFamily: {
+            sans: "'Outfit', sans-serif",
+            body: "'Outfit', sans-serif",
+            heading: "'Outfit', sans-serif",
+        },
+        fontSize: {
+            sm: ["16px", "20.16px"],
+            base: ["18px", "26px"],
+            lg: ["22px", "27.72px"],
+        },
+        extend: {
+            colors: {
+                "soft-blue": "#8BACD9",
+                cyan: "#00FFF8",
+            },
+            backgroundCOlor: {
+                main: "#0D192C",
+                card: "#15263F",
+                line: "#2E405A",
+            },
+        },
     },
     variants: {
         extend: {},
